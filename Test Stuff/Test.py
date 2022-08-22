251828a1d4abf53ae129d78140b74ba8b885ed4a
Newboy674 <Weirdgaming5@gmail.com>
2022-08-22 00:00:37 -0400

    Attempt #1 to hide the key from git using git ignore

---
+++ Test Stuff/Test.py
@@ -0,0 +1,15 @@
+
+
+
+class Question:
+    def __init__(self,question,date):
+         self.q = question
+         self.d = date
+
+q1 = Question('wats ur fav food?', 2022)
+
+print(q1.q)
+
+
+class Choice:
+    def __init__(self,option,option):
